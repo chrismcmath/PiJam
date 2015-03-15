84ee53264461d212f935acebb1050b7e7ef2a166
Chris McMath <chrismcmath@gmail.com>
2015-03-15 17:33:01 +0700

    Pi piece initial work

---
+++ unity/Assets/Scripts/Controllers/PiPieceController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PiPieceController : MonoBehaviour {
+
+    public GameObject Circle;
+    public GameObject Line;
+
+    public void Update() {
+    }
+
+    public void OnMouseDown() {
+        Debug.Log("PiPieceController OnMouseDown");
+        Toggle();
+    }
+
+    private void Toggle() {
+        Circle.SetActive(!Circle.activeSelf);
+        Line.SetActive(!Line.activeSelf);
+    }
+
+}
