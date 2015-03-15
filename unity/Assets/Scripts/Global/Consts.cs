6def721200050bde740f64fad5ea57442b9a2b38
Chris McMath <chrismcmath@gmail.com>
2015-03-15 21:21:07 +0700

    Level one test

--- unity/Assets/Scripts/Global/Consts.cs
+++ unity/Assets/Scripts/Global/Consts.cs
@@ -6,9 +6,9 @@ public class Consts : MonoBehaviour {
     public static string TAG_PIPIECE_COLLIDER = "PiPieceCollider";
     public static string LAYER_PLATFORM = "Platform";
 
-    public const float ToggleChainDelay = 0.1f;
-    public const float ToggleChainDelayReset = 0.5f; //should always be 2 * ToggleChainDelay
-    public const float PiPieceJumpForce = 1f;
+    public const float ToggleChainDelay = 0.2f;
+    public const float ToggleChainDelayReset = 1.0f; //should always be 2 * ToggleChainDelay
+    public const float PiPieceJumpForce = 10f;
     public const float AvatarSpeed = 3f;
 
 }
