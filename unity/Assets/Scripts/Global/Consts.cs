7f8d12ce9f5e6532ea8d8994c67ccdc2f25164a2
Chris McMath <chrismcmath@gmail.com>
2015-03-17 00:50:55 +0700

    Main updates

--- unity/Assets/Scripts/Global/Consts.cs
+++ unity/Assets/Scripts/Global/Consts.cs
@@ -6,10 +6,10 @@ public class Consts : MonoBehaviour {
     public static string TAG_PIPIECE_COLLIDER = "PiPieceCollider";
     public static string LAYER_PLATFORM = "Platform";
 
-    public const float ToggleChainDelay = 0.2f;
+    public const float ToggleChainDelay = 0.3f;
     public const float ToggleChainDelayReset = 1.0f; //should always be 2 * ToggleChainDelay
     public const float PiPieceJumpForce = 10f;
-    public const float PiPieceTransformPeriod = 0.2f;
+    public const float PiPieceTransformPeriod = 0.3f;
     public const float AvatarSpeed = 3f;
 
 }
