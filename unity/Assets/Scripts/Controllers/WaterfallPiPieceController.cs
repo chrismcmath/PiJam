53567280c7e1f072d3848f82255e08fd38fc8ae4
Chris McMath <chrismcmath@gmail.com>
2015-03-18 18:39:24 +0700

    More main level stuff

--- unity/Assets/Scripts/Controllers/WaterfallPiPieceController.cs
+++ unity/Assets/Scripts/Controllers/WaterfallPiPieceController.cs
@@ -3,8 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class WaterfallPiPieceController : InteractablePiPieceController {
+    private float _Counter = 20f;
     public void Update() {
         if (transform.localPosition.y < -17f) {
+            Destroy(gameObject);
+        }
+
+        _Counter -= Time.deltaTime;
+        if (_Counter < 0f) {
             Destroy(gameObject);
         }
     }
